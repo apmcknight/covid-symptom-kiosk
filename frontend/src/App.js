49dad80b0c2fbe8b69fa59484ccd9a95b5699d23
Adam McKnight <adammcknight@me.com>
2020-07-31 02:12:10 -0400

    Added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StartScreen from './components/startScreen/start';
+
+function App() {
+  return (
+    <div className="App">
+     <StartScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/startScreen/start.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import maskIcon from '../../assets/icons/mask.svg'
+import './startScreen.css';
+import '../../App.css';
+
+const StartScreen = () => {
+    return(
+        <div>
+            <img src={maskIcon} alt=""/>
+            <h1>
+                Good Morning!
+
+                {/* TODO: Add dynamic statement to say; Good morning if morning, Good Evening, if evening, etc. */}
+            </h1>
+            <p>To get started, click the start button below.</p>
+            <button className="PrimaryBtn">
+                Start Check
+            </button>
+        </div>
+    )
+}
+
+export default StartScreen;
