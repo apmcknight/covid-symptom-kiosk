d7cc42f0ef772044fe9b3a388d3e1ef58a9e2576
Adam McKnight <adammcknight@me.com>
2020-08-02 21:13:14 -0400

    Added Screening Questions component.

---
+++ frontend/src/components/screeningQuestions/screeningQuestions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Route, Router} from 'react-router'
+import Greeting from '../Greeting';
+import maskIcon from '../../assets/icons/mask.svg'
+import './screeningQuestions.css'
+import '../../App.css';
+
+const StartScreen = () => {
+    return(
+        <div>
+            <h1>Screening Questions component</h1>
+        </div>
+    )
+}
+
+export default StartScreen;
