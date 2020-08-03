bcb5ef7393d17e58999e66268b16c3e047b6e081
Adam McKnight <adammcknight@me.com>
2020-08-03 15:36:17 -0400

    PRODUCTION BUILD

---
+++ frontend/build/precache-manifest.dbee594aec44e47647667ffd4c2d42ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4e42baed040253eeb9b4fcca58f3ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7f9968591875cd6f4b5",
+    "url": "/static/css/main.40197acb.chunk.css"
+  },
+  {
+    "revision": "95dd53c3346dbc60df3d",
+    "url": "/static/js/2.08a850c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08a850c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f9968591875cd6f4b5",
+    "url": "/static/js/main.037d113a.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "f7ce95f1bec228e4d90de9b8bc07d55a",
+    "url": "/static/media/clear.f7ce95f1.svg"
+  },
+  {
+    "revision": "ea9737f46960a4d2d6b89eb611beac6a",
+    "url": "/static/media/help.ea9737f4.svg"
+  },
+  {
+    "revision": "c2816cf13ade393edcf2e8ca9c48cf1f",
+    "url": "/static/media/mask.c2816cf1.svg"
+  },
+  {
+    "revision": "eab0a4bbb9a78a4883131962ea8170e6",
+    "url": "/static/media/menu.eab0a4bb.svg"
+  },
+  {
+    "revision": "2f87c7263b21973f79c9f49105c993e7",
+    "url": "/static/media/restart.2f87c726.svg"
+  },
+  {
+    "revision": "f25fc45a4f23623dda85c656ee499f7a",
+    "url": "/static/media/sick.f25fc45a.svg"
+  }
+]);
