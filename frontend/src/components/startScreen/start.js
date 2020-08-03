e12d3579c850c4117cee1f3dc704ea374ab26e21
Adam McKnight <adammcknight@me.com>
2020-08-02 21:09:28 -0400

    Wrote javascript logic for the Greeting Component.

--- frontend/src/components/startScreen/start.js
+++ frontend/src/components/startScreen/start.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route, Router} from 'react-router'
+import Greeting from '../Greeting';
 import maskIcon from '../../assets/icons/mask.svg'
 import './startScreen.css';
 import '../../App.css';
@@ -8,10 +9,7 @@ const StartScreen = () => {
     return(
         <div>
             <img src={maskIcon} alt=""/>
-            <h1>
-                Good Morning!
-                {/* TODO: Add dynamic statement to say; Good morning if morning, Good Evening, if evening, etc. */}
-            </h1>
+            <Greeting/>
             <p>To get started, click the start button below.</p>
                 <button className="PrimaryBtn">
                     Start Check
